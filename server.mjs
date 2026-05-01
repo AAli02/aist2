88983f6f7257163da8f071197c17bccd0e946a6b
aali02 <aali.02p@gmail.com>
2026-05-01 09:19:43 -0400

    final build version

---
+++ server.mjs
@@ -0,0 +1,11 @@
+import next from "next";
+import { createServer } from "http";
+
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => handle(req, res)).listen(3000, () => {
+    console.log("Ready on http://localhost:3000");
+  });
+});
